820a2804248a32ce2a11ab7ad8876e8f123d1f2b
david-dani <davgod011@gmail.com>
2022-03-11 20:23:51 +0545

    commit

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Acer Nito Laptop',
+    image: '/images/AcerNitro.jpg',
+    description:
+      'With the combined power of a 12th Gen Intel® CoreTM i7 processor1 and NVIDIA® GeForce RTXTM 30 Series GPUs, you can rule the gaming world (fully optimized for maximum MGP). Configure your laptop with two slots for GEN 4 M.2 PCIe and up to 32GB of DDR4 3200 RAM for maximum speed and storage.',
+    brand: 'Acer',
+    category: 'Laptop',
+    price: 113000,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Acer Predator Helios Gaming',
+    image: '/images/AcerPredatorHelios.jpg',
+    description:
+      'Acer Predator Helios 300 2021 with 10th Gen Intel Core i5-10300H processor, NVIDIA GeForce RTX 3060 Graphics Card with 6GB of dedicated GDDR6 VRAM, 8GB DDR4 RAM (Expandable up to 32GB), 512GB SSD storage, Additional HDD slot for storage expansion, 15.6-inch IPS display; Full-HD (1920 x 1080 pixels) resolution; 144Hz Refresh Rate; 3ms Overdrive.',
+    brand: 'Acer',
+    category: 'Laptop',
+    price: 16500,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Asus Rog Strix Scar',
+    image: '/images/AsusRog.jpg',
+    description:
+      'Asus ROG Strix Scar 15 2021 G533QR Gaming Laptop with AMD Ryzen 9 5900HX 5000-series processor (Octa-core CPU with 16 threads), NVIDIA GeForce RTX 3070 graphics card with 8GB of GDDR6 VRAM, 32GB DDR4 RAM (Expandable), 1TB M.2 NVMe PCIe 3.0 SSD Storage, 15.6-inch IPS display, Full-HD (1920 x 1080.',
+    brand: 'Asus',
+    category: 'Laptop',
+    price: 361000,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Dell Inspiron',
+    image: '/images/DellInspiron.jpg',
+    description:
+      'Dell Inspiron 5391, Intel Core i5-10210U, 8GB RAM, 512GB SSD, NVIDIA MX250, 13.3-inch Full-HD Display, 0.59" thin, 1.24Kg weight, and "smart" Lithium-ion battery.',
+    brand: 'Dell',
+    category: 'Laptop',
+    price: 112000,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Lenovo Ideapad',
+    image: '/images/LenevoIdeapad.jpg',
+    description:
+       '14ARE05 Lenovo Ideapad 5 14ARE05 Lenovo Ideapad 5 AMD RyzenTM 7 4700U 2.0GHz 256GB SSD 8GB 14" AMD RyzenTM 7 4700U 2.0GHz (1920x1080) GRAPHITE GRAY Backlit Keyboard BT WIN10 Webcam',
+    brand: 'Lenovo',
+    category: 'Laptop',
+    price: 105000,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'HP Pavilion',
+    image: '/images/PavilionGaming.jpg',
+    description:
+      'HP Pavilion Power 15 2020 Gaming Laptop with Intel Core i5 10th Gen 10300H, NVIDIA GTX 1650, 256GB SSD 8GB, 15.6" Full-HD (1920 x 1080 pixels) display, BT WIN10 Webcam, and SHADOW BLACK Backlit Keyboard.',
+    brand: 'HP',
+    category: 'Laptop',
+    price: 112000,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
